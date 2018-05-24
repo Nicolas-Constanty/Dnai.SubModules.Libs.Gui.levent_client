7eb0543b460bc5dfafeb3c1ba012b21f85e0a054
Nicolas Constanty <nicolas.constanty@epitech.eu>
2018-05-24 17:24:42 +0200

    Add QtProject

---
+++ include/authentificationpackage.h
@@ -0,0 +1,22 @@
+#ifndef AUTHENTIFICATIONPACKAGE_H
+#define AUTHENTIFICATIONPACKAGE_H
+
+#include <QString>
+#include "headercommunication.h"
+
+PACKED(
+struct AuthentificationPackage {
+    char name[256];
+}
+);
+
+PACKED(
+struct SendAuthenticatePackage {
+    HeaderCommunication header;
+    AuthentificationPackage auth;
+}
+);
+
+void createAuthenticatePackage(SendAuthenticatePackage &, QString const &name);
+
+#endif // AUTHENTIFICATIONPACKAGE_H
